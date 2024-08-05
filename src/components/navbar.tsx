4fc65923cf0a7bb11ff958fc260c9f52f5f499a0
David <david.tomi2020@gmail.com>
2024-08-05 09:07:57 +0100

    install tailwind css

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,22 +1,11 @@
 import { Link } from "react-router-dom";
-import { Menu } from "semantic-ui-react";
 export const Navbar = () => {
-  return (
-    <Menu fixed="top" size="huge">
-      <Menu.Item as={Link} to="/" style={{ fontSize: "1.5rem" }}>
-        Home
-      </Menu.Item>
-      <Menu.Item as={Link} to="/rated" style={{ fontSize: "1.5rem" }}>
-        Rated
-      </Menu.Item>
-      <Menu.Menu position="right">
-        <Menu.Item as={Link} to="/auth" style={{ fontSize: "1.5rem" }}>
-          Auth
-        </Menu.Item>
-        <Menu.Item as={Link} to="/ark" style={{ fontSize: "1.5rem" }}>
-          Ark
-        </Menu.Item>
-      </Menu.Menu>
-    </Menu>
-  );
+  <>
+    <ul>
+      <li>Home</li>
+      <li>
+        <Link to="/">Tickets </Link>
+      </li>
+    </ul>
+  </>;
 };
